2fababb4b1438ddc09aa404ea02c9f6988b54c57
Lilla <54989842+Lilla0802@users.noreply.github.com>
2021-02-10 11:33:59 -0800

    pips

--- old.js
+++ old.js
@@ -1,132 +1,85 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-var x = myCanvas.width / 20; //These 2 variables determine the starting circles location, in this case, the top left of the screen.
-var y = myCanvas.height / 20;
-
-var dx = 0; //These variables will be used later to change the position of the circle.
-var dy = 10; //Changing both of these numbers will also change the speed of the circle (in other words, how many units the circle moves per frame).
-
+var ball = {x: c.width/20, y: c.height/20, ballSize: 5, dx: 0, dy: 10}
 var gravity = .5; //Sets the gravity pulling the ball to the ground.
 var damping = 0.75; //The rate at which the ball slows down.
-var ballSize = 5; //Sets the circle's radius.
+var newArray = [];
+var timer = 0;
+var spaceDifficulty = 400;
+var score = 0;
+/*var lowerRect = {xL: c.width - 10, yL: c.height - 100, widthL: 50, heightL: c.height};
+var upperRect = {xU: c.width - 10, yU: 0, widthU: 50, heightU: c.height - 200};
+var pipes  = {side1:upperRect.x + upperRect.y, side2 :upperRect.width + upperRect.height};
+console.log()*/
 
 function drawCircle() {//this draws the ball keeps the shap and the colors
   ctx.beginPath();
-  ctx.arc(x, y, ballSize, 0, Math.PI*2); //The circle, on frame one, will always start at the top left, and its size will always be set to ballSize.
+  ctx.arc(ball.x, ball.y, ball.ballSize, 0, Math.PI*2); //The circle, on frame one, will always start at the top left, and its size will always be set to ballSize.
   ctx.fillStyle = "#FF00FF"; //Sets the color of the circle to light blue.
   ctx.fill(); //Fills in the circle with the color provided in fillStyle.
   ctx.stroke();
 }
 
-//var rectWidth = Math.floor(Math.random() * (150 - 100) + 100);
-//var rectHeight = Math.floor(Math.random() * (200 - 150) + 150);
-//var rect = {width: rectWidth, height: rectHeight};
-//var xRange = c.width - rectWidth;
-
-//var yRange = c.height - rect.height;
-var lowerRect = {x: c.width - 10, y: c.height - 100, width: 50, height: c.height};
-var upperRect = {x: c.width - 10, y: 0, width: 50, height: c.height - 200};
-var pipes  = {side1:upperRect.x + upperRect.y, side2 :upperRect.width + upperRect.height};
-console.log()
-var newArray = [{x: c.width - 10, y: c.height - 100, width: 50, height: c.height},
-{x: c.width - 10, y: 0, width: 50, height: c.height - 200}];
-
-  function makePipe(){
-    ctx.clearRect(0, 0, c.width, c.height); //since it's a loop, this clears the canvas or else a lot of circles will be draw each time this function loops
+function makePipe(lowRectX, lowRectY, lowRectWid, lowRectHeight, upRectX, upRectY, upRectWid, upRectHeight){
+  ctx.clearRect(0, 0, c.width, c.height); //since it's a loop, this clears the canvas or else a lot of circles will be draw each time this function loops
+  for (var i = 0; i < newArray.length; i++) {
     ctx.beginPath(); //starts drawing the rectangle
-    ctx.rect(lowerRect.x, lowerRect.y, lowerRect.width, lowerRect.height);
+    ctx.rect(newArray[i].xL, newArray[i].yL, newArray[i].widthL, newArray[i].heightL);
     ctx.fillStyle = "purple"; //Sets the color of the circle to light blue.
     ctx.fill(); //Fills in the circle with the color provided in fillStyle.
     ctx.stroke(); //finish drawing the rectangle
     ctx.beginPath(); //starts drawing the rectangle
-    ctx.rect(upperRect.x, upperRect.y, upperRect.width, upperRect.height);
+    ctx.rect(newArray[i].xU, newArray[i].yU, newArray[i].widthU, newArray[i].heightU);
     ctx.fillStyle = "purple"; //Sets the color of the circle to light blue.
     ctx.fill(); //Fills in the circle with the color provided in fillStyle.
     ctx.stroke(); //finish drawing the rectangle
-    lowerRect.x = lowerRect.x - 1;
-    if ((lowerRect.x + lowerRect.width) == 0) {
-      lowerRect.x = c.width;
-    }
-    upperRect.x = upperRect.x - 1;
-    if ((upperRect.x + upperRect.width) == 0) {
-      upperRect.x = c.width;
-    }
-  }
-
-/*  var lowerRect2 = {x: c.width - 5, y: c.height - 50, width: 50, height: c.height};
-  var upperRect2 = {x: c.width - 15, y: 0, width: 100, height: c.height - 300};
-
-function Pipes2() {
-  ctx.clearRect(5,5, c.width, c.height);
-  ctx.beginPath();
-  ctx.ract(lowerRect2.x, lowerRect2.y, lowerRect2.width, lowerRect2.height);
-  ctx.fillStyle="purple";
-  ctx.fill();
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.rect(upperRect2.x, upperRect2.y, upperRect2.width, upperRect2.height);
-  ctx.fillStyle = "purple";
-  ctx.fill();
-  ctx.stroke();
-  lowerRect2.x = lowerRect2.x - 1;
-  if ((lowerRect2.x + lowerRect2.width) == 1) {
-    lowerRect2.x = c.width;
-  }
-  upperRect2.x = upperRect2.x - 1;
-  if ((upperRect2.x + upperRect2.width) == 1) {
-    upperRect.x = c.width;
   }
 }
-*/
 
-var score = 0;
+function collisionCheck() {
+  //these 2 ifs are the collision
+  if (ball.x < newArray.xL + newArray.widthL && ball.x + ball.ballSize > newArray.xL && ball.y < newArray.yL + newArray.heightL && ball.y + ball.ballSize > newArray.yL) {
+    ball.dx = -ball.dx;
+    score += 1;
+    console.log(score);
+  }
+  if (ball.x < newArray.xU + newArray.widthU && ball.x + ball.ballSize > newArray.xU && ball.y < newArray.yU + newArray.heightU && ball.y + ball.ballSize > upperRect.y) {
+    ball.dx = -ball.dx;
+    score += 1;
+    console.log(score);
+  }
+  //location.reload() if it hits something
+}
+
 function draw() {
-  var scoreBox = {x: c.width - 10, y: c.height - 200, width: 10,  height: 100};
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height); //Clears the canvas every frame, so a new circle can be drawn.
-  /*
-  ctx.beginPath();
-  ctx.fillStyle = "green";
-  ctx.fillRect(lowerRect.x, lowerRect.y, lowerRect.width, lowerRect.height);
-  ctx.fill();
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.fillStyle = "green";
-  ctx.fillRect(upperRect.x, upperRect.y, upperRect.width, upperRect.height);
-  ctx.fill();
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.fillStyle = "yellow";
-  ctx.fillRect(scoreBox.x, scoreBox.y, scoreBox.width, scoreBox.height);
-  ctx.fill();
-  ctx.stroke();
-  */
-  makePipe();
+  if (timer == spaceDifficulty) {
+    var rectArray = {xL: c.width - 10, yL: c.height - 100, widthL: 50, heightL: c.height, xU: c.width - 10, yU: 0, widthU: 50, heightU: c.height - 200};
+    newArray.push(rectArray);
+    timer = 0;
+  }
+  for (var i = 0; i < newArray.length; i++) {
+    makePipe(newArray[i].xL, newArray[i].yL, newArray[i].widthL, newArray[i].heightL, newArray[i].xU, newArray[i].yU, newArray[i].widthU, newArray[i].heightU);
+    newArray[i].xL --;
+    newArray[i].xU --;
   }
   drawCircle();
-  if (x + dx > c.width - ballSize || x + dx < ballSize) { //If the circle's x position exceeds the width of the canvas...
-    dx = -dx; //The ball's x direction will be flipped, and it will bounce a specific distance (damping).
+  if (ball.x + ball.dx > c.width - ball.ballSize || ball.x + ball.dx < ball.ballSize) { //If the circle's x position exceeds the width of the canvas...
+    ball.dx = -ball.dx; //The ball's x direction will be flipped, and it will bounce a specific distance (damping).
   }
-
-  if(y + dy > c.height - ballSize || y + dy < ballSize) { //If the circle's y position exceeds the height of the canvas...
-    dy = -dy * damping; //Its y direction will be flipped, and it's speed will decrease.
+  if(ball.y + ball.dy > c.height - ball.ballSize || ball.y + ball.dy < ball.ballSize) { //If the circle's y position exceeds the height of the canvas...
+    ball.dy = -ball.dy * damping; //Its y direction will be flipped, and it's speed will decrease.
   }
-  dy += gravity; //Adds the gravity value to the ball's dy value, giving it a artificial force of gravity.
-  x += dx;
-  if (((y + dy) + ballSize) <= c.height) {
-    y += dy;
+  ball.dy += gravity; //Adds the gravity value to the ball's dy value, giving it a artificial force of gravity.
+  ball.x += ball.dx;
+  if (((ball.y + ball.dy) + ball.ballSize) <= c.height) {
+    ball.y += ball.dy;
   }
-  //these 2 ifs are the collision
-  if (x < lowerRect.x + lowerRect.width && x + ballSize > lowerRect.x && y < lowerRect.y + lowerRect.height && y + ballSize > lowerRect.y) {
-    dx = -dx;
-    score += 1;
-    console.log(score);
+  for (var i = 0; i < newArray.length; i++) {
+    collisionCheck(newArray[i].xL, newArray[i].yL, newArray[i].widthL, newArray[i].heightL, newArray[i].xU, newArray[i].yU, newArray[i].widthU, newArray[i].heightU);
   }
-  if (x < upperRect.x + upperRect.width && x + ballSize > upperRect.x && y < upperRect.y + upperRect.height && y + ballSize > upperRect.y) {
-    dx = -dx;
-    score += 1;
-    console.log(score);
-  }
+  timer ++;
 }
 
 setInterval(draw, 15);
@@ -134,9 +87,9 @@ setInterval(draw, 15);
 document.addEventListener("keydown", makeBounce);
  function makeBounce(e) {
    if (e.key == " ") {
-     dy -= 10;
+     ball.dy -= 10;
    }
    if (e.key == "r") {
-     dx = -dx;
+     ball.dx = -ball.dx;
    }
  }
